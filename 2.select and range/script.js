2982d3f4523afbff381f69b8dcb4fcf4518d358d
taurus-git <t.1113@ya.ru>
2018-10-13 11:09:23 +0300

    simple practice with select and range

--- 2.select and range/script.js
+++ 2.select and range/script.js
@@ -1,1 +1,17 @@
-//
+/*select*/
+function fun1() {
+    var sel = document.getElementById('mySelect').selectedIndex;
+    var options = document.getElementById('mySelect').options;
+    alert('Выбрана оция ' + options[sel].text);
+}
+/*range*/
+function fun2() {
+    var rng = document.getElementById('r1');
+    var p = document.getElementById('one');
+    var i1 = document.getElementById('i1');
+    var div = document.getElementById('test');
+
+    p.innerHTML = rng.value;
+    i1.value = rng.value;
+    div.style.width = rng.value + 'px';
+}
