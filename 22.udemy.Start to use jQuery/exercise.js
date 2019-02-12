55cd01900c132696a3e5c51c9e72108db60ac4d6
taurus-git <t.1113@ya.ru>
2019-02-12 17:23:11 +0200

    Learned js methods attr() and val()

--- 22.udemy.Start to use jQuery/exercise.js
+++ 22.udemy.Start to use jQuery/exercise.js
@@ -1,4 +1,5 @@
+/*
 $("div").css("background", "purple");
 $(".hightlight").css("width", "200px");
 $("#third").css("border", "2px solid orange");
-$("div:first-of-type").css("color", "pink");
+$("div:first-of-type").css("color", "pink");*/
